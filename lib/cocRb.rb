bdbb5aa5ca59b23febaea71a3fd996c6649902d3
Enlighted <navedakther12october@gmail.com>
2021-05-05 23:11:11 +0600

    Fixing issue

--- lib/cocRb.rb
+++ lib/cocRb.rb
@@ -1,17 +1,15 @@
-require_relative 'cocRb/clan'
-require_relative 'cocRb/utility'
-require_relative 'cocRb/api'
-require_relative 'cocRb/clanWar'
-require_relative 'cocRb/gp'
-require_relative 'cocRb/labels'
-require_relative 'cocRb/league'
-require_relative 'cocRb/loactions'
-require_relative 'cocRb/player'
+require_relative './cocRb/clan'
+require_relative './cocRb/utility'
+require_relative './cocRb/api'
+require_relative './cocRb/clanWar'
+require_relative './cocRb/gp'
+require_relative './cocRb/labels'
+require_relative './cocRb/league'
+require_relative './cocRb/location'
+require_relative './cocRb/player'
 require 'faraday'
 require 'time'
 require "json"
-
- token = "eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzUxMiIsImtpZCI6IjI4YTMxOGY3LTAwMDAtYTFlYi03ZmExLTJjNzQzM2M2Y2NhNSJ9.eyJpc3MiOiJzdXBlcmNlbGwiLCJhdWQiOiJzdXBlcmNlbGw6Z2FtZWFwaSIsImp0aSI6IjA4M2E3NWQ4LTE3NGMtNDBmZC1hZjdlLWE1MGVmODEyZDU3ZiIsImlhdCI6MTU5NzIyMDQxOSwic3ViIjoiZGV2ZWxvcGVyL2Q1NWNjNDdhLTBmYjUtNDM5Yi00N2Q2LWFkZTIyMzIwMzJhYSIsInNjb3BlcyI6WyJjbGFzaCJdLCJsaW1pdHMiOlt7InRpZXIiOiJkZXZlbG9wZXIvc2lsdmVyIiwidHlwZSI6InRocm90dGxpbmcifSx7ImNpZHJzIjpbIjEwMy42MC4xNzUuNjMiXSwidHlwZSI6ImNsaWVudCJ9XX0.0r0WB3cL6rVco9DqFMnOG4s-g1sroQebE69iUHSXYepzwSc5ZDy1e4qstvAN_fOu-fN9cg37NmsAdv_elVZWpg"
 
 CocRb.configure do |config|
      config.token = token
